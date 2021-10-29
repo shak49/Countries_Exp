c10a3182dd8441080bdb5a443c39f7503cb198ae
Shak <shakfeizi@gmail.com>
2021-10-29 14:15:19 -0600

    Apollo Updates

---
+++ Countries_Exp/Countries_Exp/GraphQL/ApolloNetwork.swift
@@ -0,0 +1,21 @@
+//
+//  ApolloNetwork.swift
+//  Countries_Exp
+//
+//  Created by Shak Feizi on 10/29/21.
+//
+
+import Foundation
+import Apollo
+
+
+class NetworkCall {
+    // SHAK: Properties
+    static let shared: NetworkCall = NetworkCall()
+    lazy var apollo = ApolloClient(url: URL(string: "http://countries.trevorblades.com")!)
+    
+    private init() {}
+    
+    // SHAK: Functions
+    
+}
